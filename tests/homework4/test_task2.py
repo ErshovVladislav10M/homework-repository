a6adba27340458092cbe1daccbab7e01f2a9aa99
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-23 23:50:45 +0300

    Update test_task2.py

--- tests/homework4/test_task2.py
+++ tests/homework4/test_task2.py
@@ -1,8 +1,8 @@
+from unittest import TestCase
 from unittest.mock import patch
 
-import homework4.task_2_mock_input
 
-
-@patch("homework4.task_2_mock_input.count_dots_on_i", return_value=59)
-def test_positive(self, count_dots_on_i):
-    assert count_dots_on_i("https://example.com/") == 59
+class TestFunc(TestCase):
+    @patch("homework4.task_2_mock_input.count_dots_on_i", return_value=59)
+    def test_positive(self, count_dots_on_i):
+        assert count_dots_on_i("https://example.com/") == 59
