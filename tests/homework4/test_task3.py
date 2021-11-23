3f3003524bae956a2a623d8f3b52737206f296e8
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-23 17:23:41 +0300

    create test_task3

---
+++ tests/homework4/test_task3.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+
+from homework4.task_3_get_print_output import my_precious_logger
+
+
+def test_positive():
+    my_precious_logger("OK")
+    assert open(os.getcwd() + "/stdout_text.txt", "r").read() == "OK"
+
+
+def test_negative():
+    my_precious_logger("error: file not found")
+    assert sys.stderr == "error: file not found"
