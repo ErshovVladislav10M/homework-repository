a829be84000cccaa5beffd9ac460b6d1f853bf57
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-25 03:08:01 +0300

    Update test_task2.py

--- tests/homework4/test_task2.py
+++ tests/homework4/test_task2.py
@@ -8,7 +8,9 @@ from homework4.task_2_mock_input import count_dots_on_i
 class TestFunc(TestCase):
     @patch(
         "homework4.task_2_mock_input.get_html",
-        return_value=open(os.getcwd() + "/homework4/test_task2_html.txt", "rb").read(),
+        return_value=open(
+            os.getcwd() + "/homework4/test_task2_html.txt", "rb"
+        ).read(),
     )
     def test_positive(self, get_html):
         assert count_dots_on_i("https://example.com/") == 59
