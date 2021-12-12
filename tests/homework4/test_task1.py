211b2993d0711d4ec3ce3298fd1f89344c702b41
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-12 23:15:51 +0300

    Update test_task1.py

--- tests/homework4/test_task1.py
+++ tests/homework4/test_task1.py
@@ -30,4 +30,4 @@ def test_exception(capfd):
     read_magic_number(os.getcwd() + "/test_task1_text.txt")
     out, err = capfd.readouterr()
     os.remove(os.getcwd() + "/test_task1_text.txt")
-    assert out == "В первой строке не число"
+    assert out == "В первой строке не число\n"
