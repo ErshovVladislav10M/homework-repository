93a9ed8af9416c624bb3ac8e2e60a827a13678f6
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-12 23:06:32 +0300

    Update test_task1.py

--- tests/homework4/test_task1.py
+++ tests/homework4/test_task1.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 from homework4.task_1_read_file import read_magic_number
 
 
@@ -10,3 +12,22 @@ def test_positive():
     ans = read_magic_number(os.getcwd() + "/test_task1_text.txt")
     os.remove(os.getcwd() + "/test_task1_text.txt")
     assert ans
+
+
+
+def test_negative():
+    fi = open("test_task1_text.txt", "w")
+    fi.write("5")
+    fi.close()
+    ans = read_magic_number(os.getcwd() + "/test_task1_text.txt")
+    os.remove(os.getcwd() + "/test_task1_text.txt")
+    assert not ans
+
+
+def test_exception():
+    fi = open("test_task1_text.txt", "w")
+    fi.write("dddd")
+    fi.close()
+    out, err = capfd.readouterr()
+    os.remove(os.getcwd() + "/test_task1_text.txt")
+    assert out == "В первой строке не число"
