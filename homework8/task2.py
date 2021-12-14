08ed77ea2c832e16090ff23b5d555d064df75090
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-14 21:44:35 +0300

    Update task2.py

--- homework8/task2.py
+++ homework8/task2.py
@@ -29,15 +29,20 @@ To get a row with specific name equal to some value::
     import sqlite3
     conn = sqlite3.connect('example.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * from presidents where name=:name', {name:'Yeltsin'})
+    cursor.execute(
+        'SELECT * from presidents where name=:name',
+        {name:'Yeltsin'}
+    )
     data = cursor.fetchall()  # will get all records with this name.
     You can also use .fetchone() to get one record.
 
 in order to get record with first name (sorted alphabetically)
 use SQL expression `SELECT * from presidents order by name asc limit 1`
 in order to get record after specified (sorted alphabetically)
-use SQL expression `SELECT * from presidents where name > :name order by name limit`.
-To get amount of records in table TABLE1, use `select count(*) from TABLE1` query.
+use SQL expression
+`SELECT * from presidents where name > :name order by name limit`.
+To get amount of records in table TABLE1,
+use `select count(*) from TABLE1` query.
 
 
 Please refer to this documents for more information about
@@ -50,14 +55,20 @@ Task
 ====
 
 Write a wrapper class TableData for database table, that when initialized
-with database name and table acts as collection object (implements Collection protocol).
+with database name and table acts as collection object
+(implements Collection protocol).
 Assume all data has unique values in 'name' column.
-So, if presidents = TableData(database_name='example.sqlite', table_name='presidents')
+So, if presidents = TableData(
+    database_name='example.sqlite', table_name='presidents'
+)
 
 then
- -  `len(presidents)` will give current amount of rows in presidents table in database
- -  `presidents['Yeltsin']` should return single data row for president with name Yeltsin
- -  `'Yeltsin' in presidents` should return if president with same name exists in table
+ -  `len(presidents)` will give current amount of rows
+    in presidents table in database
+ -  `presidents['Yeltsin']` should return single data row
+    for president with name Yeltsin
+ -  `'Yeltsin' in presidents` should return
+    if president with same name exists in table
  -  object implements iteration protocol. i.e. you could use it in for loops::
        for president in presidents:
            print(president['name'])
