c792a39f4fe2f3729b75683817ac8d31c9d14706
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-16 18:38:46 +0300

    Update test_task2.py

--- tests/homework8/test_task2.py
+++ tests/homework8/test_task2.py
@@ -8,3 +8,17 @@ def test_table_data_len():
         os.getcwd() + "/homework8/" + "test_data_task2.sqlite", "presidents"
     )
     assert len(presidents) == 3
+
+
+def test_table_data_contains():
+    presidents = TableData(
+        os.getcwd() + "/homework8/" + "test_data_task2.sqlite", "presidents"
+    )
+    assert "Trump" in presidents
+
+
+def test_table_data_iterable():
+    presidents = TableData(
+        os.getcwd() + "/homework8/" + "test_data_task2.sqlite", "presidents"
+    )
+    assert [president[0] for president in presidents] == ['Yeltsin', 'Trump', 'Big Man Tyrone']
