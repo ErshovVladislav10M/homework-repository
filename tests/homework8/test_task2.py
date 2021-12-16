8f78b7245a26f49c8d64903ed16adf50b405a95c
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-16 18:35:30 +0300

    Update test_task2.py

--- tests/homework8/test_task2.py
+++ tests/homework8/test_task2.py
@@ -5,6 +5,6 @@ from homework8.task2 import TableData
 
 def test_table_data_len():
     presidents = TableData(
-        os.getcwd() + "/homework2/" + "test_data_task2.sqlite", "presidents"
+        os.getcwd() + "/homework8/" + "test_data_task2.sqlite", "presidents"
     )
     assert len(presidents) == 3
