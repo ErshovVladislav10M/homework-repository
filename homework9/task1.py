48f7e16be8a5f5fdbc299132f66693ab1adc751b
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-21 00:39:42 +0300

    create test_task1

--- homework9/task1.py
+++ homework9/task1.py
@@ -14,9 +14,18 @@ file2.txt:
 >>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
 [1, 2, 3, 4, 5, 6]
 """
-from pathlib import Path
-from typing import Iterator, List, Union
+from typing import Iterator
 
 
-def merge_sorted_files(file_list: List[Union[Path, str], ...]) -> Iterator:
-    pass
+def merge_sorted_files(file_list) -> Iterator:
+    all_numbers_from_files = []
+    for file_name in file_list:
+        with open(file_name) as fi:
+            all_numbers_from_files.append(fi.read().split())
+
+    sorted_numbers = []
+    for j in range(len(all_numbers_from_files[0])):
+        for i in range(len(all_numbers_from_files)):
+            sorted_numbers.append(int(all_numbers_from_files[i][j]))
+
+    return iter(sorted_numbers)
