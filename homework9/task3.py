8c5b1049de7705d888fc12eea8f91253fc1da185
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-22 00:06:46 +0300

    update task3

--- homework9/task3.py
+++ homework9/task3.py
@@ -10,6 +10,7 @@ For dir with two files from hw1.py:
 6
 
 """
+import tokenize
 from pathlib import Path
 from typing import Callable, Optional
 
@@ -18,7 +19,16 @@ def universal_file_counter(
     dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
 ) -> int:
     counter = 0
-    for file in dir_path.glob(file_extension):
-        with open(file) as fi:
-            counter += sum(1 for _ in fi)
+    if tokenizer:
+        for file in dir_path.glob(file_extension):
+            with open(file, "rb") as fi:
+                counter += sum(
+                    1
+                    for token in tokenize.tokenize(fi.readline)
+                    if token.exact_type == 4
+                )
+    else:
+        for file in dir_path.glob(file_extension):
+            with open(file) as fi:
+                counter += sum(1 for _ in fi)
     return counter
--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -17,3 +17,18 @@ def test_universal_file_counter_without_tokenizer():
 
     os.remove(os.getcwd() + "/file1.txt")
     os.remove(os.getcwd() + "/file2.txt")
+
+
+def test_universal_file_counter_with_tokenizer():
+    fi = open(os.getcwd() + "/file1.txt", "w")
+    fi.write("1\n3\n5\n")
+    fi.close()
+    fi = open(os.getcwd() + "/file2.txt", "w")
+    fi.write("2\n4\n6\n")
+    fi.close()
+
+    lines = universal_file_counter(Path.cwd(), "*.txt", str.split)
+    assert lines == 6
+
+    os.remove(os.getcwd() + "/file1.txt")
+    os.remove(os.getcwd() + "/file2.txt")
