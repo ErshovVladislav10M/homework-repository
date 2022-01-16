d7ae06268e27f1948f55a12a049f8e0cd92ed55e
ErshovVladislav10M <yersh2000@gmail.com>
2022-01-16 16:38:30 +0300

    Update test_task3.py

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -4,25 +4,81 @@ from homework9.task3 import universal_file_counter
 
 
 def test_universal_file_counter_without_tokenizer():
-    fi = open(Path.cwd() / "file1.txt", "w")
+    fi = open(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file1.txt",
+        "w",
+    )
     fi.write("1\n3\n5\n")
     fi.close()
-    fi = open(Path.cwd() / "file2.txt", "w")
+    fi = open(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file2.txt",
+        "w",
+    )
     fi.write("2\n4\n6\n")
     fi.close()
 
-    Path.mkdir(Path.cwd() / "ddd")
-    fi = open(Path.cwd() / "ddd" / "file2.txt", "w")
+    Path.mkdir(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "ddd"
+    )
+    fi = open(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "ddd"
+        / "file2.txt",
+        "w",
+    )
     fi.write("2\n4\n6\n")
     fi.close()
 
-    lines = universal_file_counter(Path.cwd(), "*.txt")
+    lines = universal_file_counter(
+        Path.cwd().parent / "homework-repository" / "tests" / "homework9",
+        "*.txt",
+    )
     assert lines == 9
 
-    Path.unlink(Path.cwd() / "file1.txt")
-    Path.unlink(Path.cwd() / "file2.txt")
-    Path.unlink(Path.cwd() / "ddd" / "file2.txt")
-    Path.rmdir(Path.cwd() / "ddd")
+    Path.unlink(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file1.txt"
+    )
+    Path.unlink(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file2.txt"
+    )
+    Path.unlink(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "ddd"
+        / "file2.txt"
+    )
+    Path.rmdir(
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "ddd"
+    )
 
 
 def test_universal_file_counter_with_tokenizer():
