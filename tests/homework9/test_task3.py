297fa3d893ab028e97028f5fee42500ae5559f74
ErshovVladislav10M <yersh2000@gmail.com>
2022-01-16 17:20:36 +0300

    Update test_task3.py

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -83,44 +83,24 @@ def test_universal_file_counter_without_tokenizer():
 
 def test_universal_file_counter_with_tokenizer():
     fi = open(
-        Path.cwd().parent
-        / "homework-repository"
-        / "tests"
-        / "homework9"
-        / "file1.txt",
+        Path.cwd() / "tests" / "homework9" / "file1.txt",
         "w",
     )
     fi.write("1\n3\n5\n")
     fi.close()
     fi = open(
-        Path.cwd().parent
-        / "homework-repository"
-        / "tests"
-        / "homework9"
-        / "file2.txt",
+        Path.cwd() / "tests" / "homework9" / "file2.txt",
         "w",
     )
     fi.write("2\n4\n6\n")
     fi.close()
 
     lines = universal_file_counter(
-        Path.cwd().parent / "homework-repository" / "tests" / "homework9",
+        Path.cwd() / "tests" / "homework9",
         "*.txt",
         str.split,
     )
     assert lines == 6
 
-    Path.unlink(
-        Path.cwd().parent
-        / "homework-repository"
-        / "tests"
-        / "homework9"
-        / "file1.txt"
-    )
-    Path.unlink(
-        Path.cwd().parent
-        / "homework-repository"
-        / "tests"
-        / "homework9"
-        / "file2.txt"
-    )
+    Path.unlink(Path.cwd() / "tests" / "homework9" / "file1.txt")
+    Path.unlink(Path.cwd() / "tests" / "homework9" / "file2.txt")
