ecf7634f31be248c3282fc7513c5d9776ce886b7
ErshovVladislav10M <yersh2000@gmail.com>
2022-01-16 20:31:50 +0300

    Update task1.py

--- homework9/task1.py
+++ homework9/task1.py
@@ -14,28 +14,19 @@ file2.txt:
 >>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
 [1, 2, 3, 4, 5, 6]
 """
+import heapq
+from contextlib import ExitStack
 from typing import Iterator
 
 
 def merge_sorted_files(file_list) -> Iterator:
-    all_numbers_from_files = []
-    for file_name in file_list:
-        with open(file_name) as fi:
-            all_numbers_from_files.append(fi.read().split())
-
-    sorted_numbers = []
-    while len(all_numbers_from_files) > 0:
-        min_num = min(
-            list(
-                enumerate(
-                    ([int(numbers[0]) for numbers in all_numbers_from_files]), 0
-                )
-            ),
-            key=lambda x: x[1],
+    with ExitStack() as stack:
+        files = [
+            stack.enter_context(open(file_name)) for file_name in file_list
+        ]
+        return iter(
+            [
+                int(num.split()[0])
+                for num in heapq.merge(*files, key=lambda x: int(x.split()[0]))
+            ]
         )
-        sorted_numbers.append(min_num[1])
-        all_numbers_from_files[min_num[0]].remove(str(min_num[1]))
-        if len(all_numbers_from_files[min_num[0]]) == 0:
-            all_numbers_from_files.remove(all_numbers_from_files[min_num[0]])
-
-    return iter(sorted_numbers)
