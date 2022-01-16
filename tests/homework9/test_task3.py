2744a99efdc99608efd997d33ce3d3a0035d2f68
ErshovVladislav10M <yersh2000@gmail.com>
2022-01-16 16:36:29 +0300

    Update test_task3.py

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -27,24 +27,44 @@ def test_universal_file_counter_without_tokenizer():
 
 def test_universal_file_counter_with_tokenizer():
     fi = open(
-        Path.cwd().parent / "homework-repository" / "tests" / "file1.txt", "w"
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file1.txt",
+        "w",
     )
     fi.write("1\n3\n5\n")
     fi.close()
     fi = open(
-        Path.cwd().parent / "homework-repository" / "tests" / "file2.txt", "w"
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file2.txt",
+        "w",
     )
     fi.write("2\n4\n6\n")
     fi.close()
 
     lines = universal_file_counter(
-        Path.cwd().parent / "homework-repository" / "tests", "*.txt", str.split
+        Path.cwd().parent / "homework-repository" / "tests" / "homework9",
+        "*.txt",
+        str.split,
     )
     assert lines == 6
 
     Path.unlink(
-        Path.cwd().parent / "homework-repository" / "tests" / "file1.txt"
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file1.txt"
     )
     Path.unlink(
-        Path.cwd().parent / "homework-repository" / "tests" / "file2.txt"
+        Path.cwd().parent
+        / "homework-repository"
+        / "tests"
+        / "homework9"
+        / "file2.txt"
     )
